31d0d529b94f48c761984763750d5e245c5f94ce
MikiasTeshome <49233261+MikiasTeshome@users.noreply.github.com>
2019-06-21 15:36:06 +0300

    expert side

---
+++ expert/chat.dart
@@ -0,0 +1,302 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+
+final ThemeData iOSTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData defaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
+
+// void main() {
+//   runApp(FriendlyChatApp());
+// }
+
+// class FriendlyChatApp extends StatelessWidget {
+  // String fullName;
+  // FriendlyChatApp(this.fullName);
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "FriendlyChat",
+//       home: ChatScreen(),
+//       theme:
+//           defaultTargetPlatform == TargetPlatform.iOS ? iOSTheme : defaultTheme,
+//     );
+//   }
+// }
+
+class ChatScreen extends StatefulWidget {
+    String fullName;
+  ChatScreen(this.fullName);
+  @override
+  State<StatefulWidget> createState() {
+    return ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  static final url = 'http://'+url+':8000/api/users';
+   List data;
+   var datastring;
+
+
+  var test;
+  Future<String> getData() async {
+    var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    this.setState(() {
+      // test = jsonDecode(response.body);
+      data = jsonDecode(response.body);
+      datastring = data.toString();
+    });
+    print(data);
+    print(datastring);
+
+    
+
+    return "sccess";
+  }
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final List<ChatMessageRecieved> _messagesrecieved = <ChatMessageRecieved>[];
+
+  final TextEditingController _textEditingController = TextEditingController();
+  bool _isComposing = false;
+  @override
+  void initState() {
+    super.initState();
+    getData();
+        Text(getData().toString());
+  }
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(
+        color: Theme.of(context).accentColor,
+      ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textEditingController,
+                onChanged: (String text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    InputDecoration.collapsed(hintText: "Send a message " + widget.fullName),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? CupertinoButton(
+                      child: Text("Send" ),
+                      onPressed: _isComposing
+                          ? () => _handleSubmitted(_textEditingController.text)
+                          : null,
+                    )
+                  : IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: _isComposing
+                          ? () => _handleSubmitted(_textEditingController.text)
+                          : null,
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textEditingController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+    ChatMessage message = ChatMessage(
+      text:text,
+      animationController: AnimationController(
+          duration: Duration(milliseconds: 320), vsync: this),fullName: "Dr" + " "+widget.fullName,
+
+    );
+     ChatMessageRecieved messagerecived = ChatMessageRecieved(
+      text: widget.fullName,
+      animationController: AnimationController(
+          duration: Duration(milliseconds: 320), vsync: this),fullName: "user" ,
+
+    );
+    setState(() {
+      _messages.insert(0, message);
+      _messagesrecieved.insert(0, messagerecived);
+
+    });
+    message.animationController.forward();
+    messagerecived.animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color greencolor = Color(0xFF26C6DA);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: greencolor,
+        title: Text("Pma chat"),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+           Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.fromLTRB(150, 0, 0, 0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messagesrecieved[index],
+              itemCount: _messagesrecieved.length,
+            ),
+          ),
+          
+          Divider(
+            height: 1.0,
+          ),
+          Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: _buildTextComposer()),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages)
+      message.animationController.dispose();
+    super.dispose();
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.animationController,this.fullName});
+
+  final String text;
+  final AnimationController animationController;
+final String fullName;
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+      sizeFactor:
+          CurvedAnimation(parent: animationController, curve: Curves.easeInOut),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(
+                child: Text(text[0]),
+              ),
+            ),
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    fullName ,
+                    style: Theme.of(context).textTheme.subhead,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(text),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class ChatMessageRecieved extends StatelessWidget {
+    static final url = 'http://'+url+':8000/api/blog';
+   List data;
+
+
+  var test;
+  Future<String> getData() async {
+    var response = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    // this.setState(() {
+      // test = jsonDecode(response.body);
+      data = jsonDecode(response.body);
+    // });
+    print(data);
+    return "sccess";
+  }
+  ChatMessageRecieved({this.text, this.animationController,this.fullName});
+
+  final String text;
+  final AnimationController animationController;
+final String fullName;
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+      sizeFactor:
+          CurvedAnimation(parent: animationController, curve: Curves.easeInOut),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    fullName ,
+                    style: Theme.of(context).textTheme.subhead,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(data.toString()),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(right: 0.0),
+              child: CircleAvatar(
+                child: Text(text[0]),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ expert/login.dart
@@ -0,0 +1,228 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutterpma/expert/home.dart';
+import 'package:flutterpma/expert/signup.dart';
+import 'package:flutterpma/user/homeuser.dart';
+import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
+
+import '../main.dart';
+
+
+
+class Post {
+  final String email;
+  final String password;
+
+  Post({this.email, this.password});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      email: json['email'],
+      password: json['password'],
+    );
+  }
+
+  Map toMap() {
+    var map = new Map<String, dynamic>();
+    map["email"] = email;
+    map["password"] = password;
+    // map["body"] = body;
+
+    return map;
+  }
+}
+
+Future<Post> createPost(String url, {Map body}) async {
+  var data;
+
+  return http.post(url, body: body).then((http.Response response) {
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      throw new Exception("Error while fetching data");
+    }
+    data = jsonDecode(response.body);
+    //save the access toke everytime
+    // print(data);
+    // print(data['user']['role']);
+    // print(response.body.contains(''));
+
+    return Post.fromJson(json.decode(response.body));
+  });
+}
+
+
+class Login extends StatelessWidget {
+    var data;
+  Future<Post> createPost(String url, {Map body}) async {
+
+
+  return http.post(url, body: body).then((http.Response response) {
+    final int statusCode = response.statusCode;
+
+    if (statusCode < 200 || statusCode > 400 || json == null) {
+      throw new Exception("Error while fetching data");
+    }
+    data = jsonDecode(response.body);
+    //save the access toke everytime
+    // print(data);
+    print(data['user']['role']);
+    // print(response.body.contains(''));
+
+    return Post.fromJson(json.decode(response.body));
+  });
+}
+
+    static final LOGINURL = 'http://'+url+':8000/api/auth/login';
+static final role = 'http://'+url+':8000/api/user/expert';
+  // var data ;
+  // Future<String> getData() async {
+
+  //   var response = await http.get(Uri.encodeFull(role),
+  //       headers: {"Accept": "application/json"});
+  //     // test = jsonDecode(response.body);
+  //     data = jsonDecode(response.body);
+  //   print(data.toString().contains('role'));  
+
+  //   print(data);
+  //   return "sccess";
+  // }
+
+
+  TextEditingController emailcontroler = new TextEditingController();
+  TextEditingController passwordcontroler = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    bool _acceptterms = false;
+
+    // TODO: implement build
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          new Container(
+            margin: new EdgeInsets.all(20.0),
+            child: CircleAvatar(
+              radius: 55,
+              child: Image.asset('assets/hospital.jpg'),
+            ),
+          ),
+            new Container(
+            margin: new EdgeInsets.all(20.0),
+              child: Center(child: Text('Sign in',style: TextStyle(fontSize: 30),),),
+          ),
+          Container(
+            margin: new EdgeInsets.only(left: 50.0, right: 50.0),
+            child: Column(
+              children: <Widget>[
+                new TextField(
+                  controller: emailcontroler,
+                  maxLines: 1,
+                  style: Theme.of(context).textTheme.title,
+                  decoration: new InputDecoration(
+                    labelText: "Email",
+                    // isDense: true
+                  ),
+                ),
+                new TextField(
+                                obscureText: true,
+
+                  controller: passwordcontroler,
+                  maxLines: 1,
+                  style: Theme.of(context).textTheme.title,
+                  decoration: new InputDecoration(
+                    labelText: "password",
+                    
+                    // isDense: true
+                  ),
+                ),
+                SwitchListTile(
+                    onChanged: (bool value) {
+                      _acceptterms = value;
+                    },
+                    value: _acceptterms,
+                    title: Text("Accept Terms of PMA")),
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: RaisedButton(
+                    color: Colors.greenAccent,
+               onPressed: () async {
+                Post newPost = new Post(
+                  email: emailcontroler.text,
+                  password: passwordcontroler.text,
+                );
+                Post p =
+                    await createPost(LOGINURL, body: newPost.toMap())
+                        .then((_) => newPost)
+                        .catchError((e) {
+                  print(e); // Finally, callback fires.
+                  return Fluttertoast.showToast(
+                      msg: "Incorrect email or password",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.BOTTOM,
+                      timeInSecForIos: 1,
+                      backgroundColor: Colors.red,
+                      textColor: Colors.white,
+                      fontSize: 16.0); // Future completes with 42.
+                }).then((value) {
+                  print(data);
+                  
+                  if (data['user']['role']=='expert') {
+                    Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                          // builder: (context) => HomePageExpert(newPost.email),
+                          // builder: (context) => HomePageUser('user'),
+                          builder: (context) => HomePageExpert(email:newPost.email,id: data['user']['id'],)
+                        
+
+
+                        ));
+                  } else {
+                    Navigator.push(
+                        context,
+                        new MaterialPageRoute(
+                          // builder: (context) => HomePageUser('user'),
+                          builder: (context) => HomePageUser(email:newPost.email,userinfo: data),
+
+
+                        ));
+                  }
+                  ;
+                });
+                      // Navigator.push(
+                      //     context,
+                      //     new MaterialPageRoute(
+                      //       builder: (context) => HomePage("Expert"),
+                      //     ));
+                    },
+                    textTheme: ButtonTextTheme.primary,
+                    child: Text(
+                      "Login",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 13,
+                ),
+                new Container(
+                
+                  child: GestureDetector(
+                    child: Text("Not a memeber Sign up"),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/signupexpert');
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
